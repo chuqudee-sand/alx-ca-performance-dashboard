c46b032c871edb7b1b9ce304e2b2693562cf8aaa
chuqudee-sand <cokereafor@sandtech.com>
2026-02-28 08:25:34 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,15 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "node_modules/@tremor/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx,mdx}", // This line is vital for the cards!
   ],
   theme: {
     extend: {
       colors: {
-        berkeleyBlue: "#002B56",
-        springGreen: "#05F283",
-        iris: "#5648B7",
-        tomato: "#FF5347",
+        berkeleyBlue: '#002B56',
+        springGreen: '#05F283',
+        iris: '#5648B7',
+        tomato: '#FF5347',
       },
     },
   },
