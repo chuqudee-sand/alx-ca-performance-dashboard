280a6ea7b309a8e1991d2c9fa843ab7998f2017b
chuqudee-sand <cokereafor@sandtech.com>
2026-02-28 07:18:06 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/@tremor/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
-        berkeleyBlue: '#002B56',
-        springGreen: '#05F283',
-        iris: '#5648B7',
-        electricBlue: '#27DEF2',
-        tomato: '#FF5347',
+        berkeleyBlue: "#002B56",
+        springGreen: "#05F283",
+        iris: "#5648B7",
+        tomato: "#FF5347",
       },
     },
   },
+  plugins: [],
 }
