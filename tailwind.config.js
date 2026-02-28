64e1283616cf023ac941720c6cbc2e070ae3e7c2
chuqudee-sand <cokereafor@sandtech.com>
2026-02-28 07:05:21 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        berkeleyBlue: '#002B56',
+        springGreen: '#05F283',
+        iris: '#5648B7',
+        electricBlue: '#27DEF2',
+        tomato: '#FF5347',
+      },
+    },
+  },
+}
